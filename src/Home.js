582d60f4a20b1d6bf745c902ddd8abd4092dcc0b
nguyed8 <145173728+nguyed8@users.noreply.github.com>
2024-06-14 15:18:03 -0400

    added some files

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,37 @@
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
-    const handleClick = (e) => {
-        console.log('hello everyone', e);
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target)
-    }
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('could not fetch the data for that resource');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data)
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message)
+                })
+        }, 1000);
+    }, []);
 
     return (  
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('Doan', e)}>Click me again</button>
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
 }
